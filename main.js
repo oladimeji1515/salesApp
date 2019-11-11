ebd6b9554c8bafc20f7190701021bd5775131e4f
oladimeji <oladimejidaniel255@gmail.com>
2019-11-11 08:40:35 -0500

    ready for push to our github repo

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ function pay() {
     var DES = document.getElementById("comment").value;
     document.getElementById("DES").innerHTML = DES;
 }
+function num(){
+var num ="";
+var copy = "Photocopy";
+var numm = 4;
+var amt = 200;
+var mop = "Cash";
+
+var serial = [];
+serial.push({Number:numm,category:copy,amount:amt,MOP:mop});
+
+/*for(i=0; i < serial.length; i++ ){
+num +=serial[i] + "<br>";
+
+
+}*/
+for(i=0; i < serial.length; i++){
+num +="<tr><td>" + Number(i+1) + "</td><td>"+serial[i].category+"</td><td>"+serial[i].amount+"</td><td>"+serial[i].MOP+"</td></tr>";
+}
+document.getElementById("demo").innerHTML = num;}
